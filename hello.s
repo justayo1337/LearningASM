2b414ee4ca77210d48313f14501aef20e39a2576
justayo1337 <ayoto.adetunji@gmail.com>
2025-04-05 12:56:07 -0400

    shellcode

--- hello.s
+++ hello.s
@@ -7,18 +7,23 @@ section .data
 section .text
 _start:
 	;usually would want to push a null byte 00 \x00 0x00 in too 
-	mov rax,1
-	mov rdi,1	
-	mov rbx, 'y!'
+	xor rax, rax
+	mov al,1
+	xor rdi, rdi
+	mov dil,1	
+	xor rbx,rbx
+	mov bx, 'y!'
 	push rbx
 	mov rbx, 'B Academ'
 	push rbx
 	mov rbx, 'Hello HT'
 	push rbx
 	mov rsi,rsp
-	mov rdx,18
+	xor rdx,rdx
+	mov dl,18
 	syscall
-
-	mov rax,60
-	mov rdi,0
+	
+	xor rax,rax
+	mov al,60
+	xor rdi, rdi
 	syscall
--- print_shellcode.py
+++ print_shellcode.py
@@ -11,4 +11,5 @@ file = ELF(sys.argv[1])
 shellcode=file.section(".text")
 print(shellcode.hex())
 
+print("%d bytes - Found NULL byte" % len(shellcode)) if [i for i in shellcode if i == 0] else print("%d bytes - No NULL bytes" % len(shellcode))
 
